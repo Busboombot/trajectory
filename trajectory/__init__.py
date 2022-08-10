80379242ccc8c0feb0540f9a35f0547f98ebc45d
Eric Busboom <eric@civicknowledge.com>
2022-08-10 12:50:07 -0700

    .

---
+++ trajectory/__init__.py
@@ -0,0 +1,6 @@
+
+from .segments import ( SegmentList, Segment, Joint, JointSegment, SegmentError, SimSegment)
+
+from .joystick import PygameJoystick as Joystick
+
+
---
+++ trajectory/old/util.py
@@ -0,0 +1,37 @@
+
+
+import struct
+
+def sign(a): return (a>0) - (a<0)
+
+
+
+
+def s32tou(v):
+    """Convert a signed 32 bit in to unsigned """
+    return struct.unpack('I', struct.pack('i', v))[0]
+
+def _find_getch():
+    try:
+        import termios
+    except ImportError:
+        # Non-POSIX. Return msvcrt's (Windows') getch.
+        import msvcrt
+        return msvcrt.getch
+
+    # POSIX system. Create and return a getch that manipulates the tty.
+    import sys, tty
+    def _getch():
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(fd)
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+    return _getch
+
+getch = _find_getch()
+
